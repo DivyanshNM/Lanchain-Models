e440981f686b4d2b3eb6cfc3f703e728fdfc9d59
DivyanshNM <www.divyanshmishra2005@gmail.com>
2026-06-10 12:29:23 +0530

    Included session wise memory

--- ChatBot/chatBot.py
+++ ChatBot/chatBot.py
@@ -1,4 +1,5 @@
 from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.chat_history import InMemoryChatMessageHistory
 from dotenv import load_dotenv
 import os
@@ -12,14 +13,23 @@ llm=HuggingFaceEndpoint(
 )
 history=InMemoryChatMessageHistory()
 model=ChatHuggingFace(llm=llm)
+prompt=ChatPromptTemplate.from_messages([
+    ("system","You are a helpful and experienced Indian Advocate."),
+    MessagesPlaceholder(variable_name="chat_history"),
+    ("human","{input}")
+])
+chain =prompt|model
 print("chatBot started (type 'exit' to quit)\n")
 
 while True:
     user_input=input("you: ")
     if user_input.lower()=="exit":
         break
-    history.add_user_message(user_input)
-    response=model.invoke(history.messages)
+    response=chain.invoke({
+        "chat_history":history.messages,
+        "input":user_input
+    })
 
+    history.add_user_message(user_input)
     history.add_ai_message(response.content)
     print(f"Bot: {response.content}\n")
